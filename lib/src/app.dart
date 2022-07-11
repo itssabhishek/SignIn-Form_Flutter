45ab0321f0837e0cd389eede2ea271d60a5c49bd
itssabhishek <itsabhishek0710@gmail.com>
2022-07-11 19:57:17 +0530

    Initail Commit

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Log Me In!',
+      home: Scaffold(
+        body: Text('Show a form here!'),
+      ),
+    );
+  }
+}
